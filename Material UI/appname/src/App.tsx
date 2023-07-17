dc4bcb94e15eb557908a21a8e52e3a055c19d1f4
ShubhamSukum <shubhamsukum1722002@gmail.com>
2023-07-17 19:43:24 +0530

    Learning material UI - Typography

---
+++ Material UI/appname/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+// import {Muitopography} from "./components/muitopography";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Muitopography/> */}
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ Material UI/appname/src/components/muitopography.tsx
@@ -0,0 +1,30 @@
+import {Typography} from "@mui/material";
+
+export const Muitopography = () => {
+  return (
+    <div>
+        <Typography variant="subtitle1">Subtitle 1 with space doesn't work</Typography>
+        <Typography variant="subtitle2">subtitle2 without space works</Typography>
+
+        <Typography variant="h1">H1 Heading</Typography>
+        <Typography variant="h2">H2 Heading</Typography>
+        <Typography variant="h3">H3 Heading</Typography>
+        <Typography variant="h4" component="h1">H4 Heading</Typography>
+        <Typography variant="h5">H5 Heading</Typography>
+        <Typography variant="h6" gutterBottom>H6 Heading</Typography>
+
+        <Typography variant="body1" gutterBottom>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis perspiciatis officiis labore dolore ea unde blanditiis hic omnis? Praesentium voluptate quia molestiae deleniti numquam facere atque eos maxime quo labore quidem necessitatibus non, natus exercitationem consequatur vel quos repellendus, similique animi hic. Quae quo veniam inventore recusandae earum aut reiciendis, officia dolorem velit temporibus rem et sequi quos asperiores ipsum molestias cum nostrum omnis harum incidunt unde ab aliquid, magnam soluta. Numquam blanditiis quam nemo, assumenda ad aut animi obcaecati enim maxime quo quidem iste quasi harum quas iusto illo. Officia quas perferendis ratione tempora suscipit a inventore quos expedita!
+        </Typography>
+
+        <Typography variant="body2">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore vitae enim est id ratione dolore. Natus incidunt eos fuga at dolorum fugit, autem alias ab recusandae, unde eius a, assumenda consequuntur eveniet nesciunt quisquam inventore itaque ipsam praesentium pariatur consectetur exercitationem! Suscipit, nobis. Soluta perspiciatis repellat, reiciendis aliquid harum beatae voluptate molestias a nihil, non et. Quam suscipit nobis aspernatur iusto, facere totam veritatis. At, animi sapiente, reiciendis dolorum veritatis laudantium quo asperiores neque quae vel dolorem ut commodi deleniti, enim laboriosam consequatur nesciunt mollitia cum officia voluptate. Nam ducimus earum dignissimos pariatur sed, nihil minima neque sit quisquam aliquam?
+        </Typography>
+
+
+
+    </div>
+  )
+}
+
+export default Muitopography
