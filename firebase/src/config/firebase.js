308418fcba3bd16ba516cf668cb0ca15e28c99e1
ShubhamSukum <shubhamsukum1722002@gmail.com>
2023-08-15 22:30:19 +0530

    learning firebase

---
+++ firebase/src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAaVMKxbVp8KqduxG2sR72uo6NwPlJtO7A",
+  authDomain: "fir-pedro-b1990.firebaseapp.com",
+  projectId: "fir-pedro-b1990",
+  storageBucket: "fir-pedro-b1990.appspot.com",
+  messagingSenderId: "348159970520",
+  appId: "1:348159970520:web:4863b1535fb72231541bad",
+  measurementId: "G-7E6332ZV41"
+};
+
+const app = initializeApp(firebaseConfig);
