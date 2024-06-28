c3b98d124d1c04fc0253d31d48d3bc9773a36f74
Shubham Sukum <shubhamsukum1722002@gmail.com>
2024-06-28 16:01:27 +0530

    env variable testing

--- Docker/main.js
+++ Docker/main.js
@@ -1,11 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app=express();
 
+dotenv.config();
+const shubz = process.env.shubz;
+
 app.use(express.json());
 
-app.get("/",(req,res)=>res.json("Docker Server is up !!"));
+app.get("/",(req,res)=>res.json("Docker Server is up !!"+shubz));
 
 app.listen(3001,()=>{
-    console.log("Server running!!");
+    console.log("Server running!!"+shubz);
 })
